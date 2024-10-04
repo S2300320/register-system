c0f0b573cfe2571ad18e818ffbadf74ebe1e599d
Alpha Bah <S2300320@ndonline.ac.uk>
2024-10-04 12:56:37 +0100

    Add project files.

---
+++ algorithms/Program.cs
@@ -0,0 +1,45 @@
+using algorithms.Components;
+using algorithms.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace algorithms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddDbContext<TlS2300320RzaContext>(options =>
+               options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
+               new MySqlServerVersion(new Version(8, 0, 29))));
+
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
